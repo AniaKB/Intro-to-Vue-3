10a28454569ff414db9e03ab922210c0eb3824ba
Gazarino <aniakbennett@gmail.com>
2021-02-21 15:53:24 -0500

    adding product photo using Vue

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 const app = Vue.createApp({
     data() {
         return {
-            product: 'Socks'
-        }
+            product: 'Socks',
+            image: './assets/images/socks_green.jpg',
+            url: 'https://www.google.com/search?q=socks&client=firefox-b-1-d&source=lnms&tbm=isch&sa=X&ved=2ahUKEwiYyKLB6_vuAhUxB50JHWOwCVwQ_AUoAnoECAUQBA&biw=667&bih=786'
+        };
     }
-})
+});
